c54226a967a8ea08be3b17e0c539eef7baefea40
RFLac <rogerflacerda@gmail.com>
2024-02-13 19:35:09 -0300

    variável de email adicinada ao formulário

--- src/components/auth/PasswordRecoverForm.tsx
+++ src/components/auth/PasswordRecoverForm.tsx
@@ -1,37 +1,77 @@
-import { Button, Grid, InputLabel, OutlinedInput, Stack } from "@mui/material"
-import { AnimateButton } from "../shared/elements/AnimatedButton"
-
+import { Button, Grid, InputLabel, OutlinedInput, Stack } from "@mui/material";
+import { AnimateButton } from "../shared/elements/AnimatedButton";
+import React from "react";
 
 export const PasswordRecoverForm = () => {
-    return (
-        <>
-            <form>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="email-recuperar-senha">E-mail</InputLabel>
-                            <OutlinedInput
-                                id="email-recuperar-senha"
-                                type="email"
-                                name="email"
-                                placeholder="Informe seu email"
-                            />
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <AnimateButton>
-                            <Button fullWidth size="large" type="submit" variant="contained" color="primary" sx={{ marginBottom: 3 }}>
-                                Enviar
-                            </Button>
-                        </AnimateButton>
-                        <AnimateButton>
-                            <Button fullWidth size="large" type="button" variant="outlined" color="secondary" href="/auth/login">
-                                Cancelar
-                            </Button>
-                        </AnimateButton>
-                    </Grid>
-                </Grid>
-            </form>
-        </>
-    )
-}
+  const [email, setEmail] = React.useState<string>("");
+  return (
+    <>
+      <form>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Stack spacing={1}>
+              <InputLabel htmlFor="email-recuperar-senha">E-mail</InputLabel>
+              <OutlinedInput
+                id="email-recuperar-senha"
+                type="email"
+                name="email"
+                placeholder="Informe seu email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Stack>
+          </Grid>
+          <Grid item xs={12}>
+            <AnimateButton>
+              <Button
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+                color="primary"
+                sx={{ marginBottom: 3 }}
+              >
+                Enviar
+              </Button>
+            </AnimateButton>
+            <AnimateButton>
+              <Button
+                fullWidth
+                size="large"
+                type="button"
+                variant="outlined"
+                color="secondary"
+                href="/auth/login"
+              >
+                Cancelar
+              </Button>
+            </AnimateButton>
+          </Grid>
+        </Grid>
+      </form>
+    </>
+  );
+};
+
+// const sendPasswordRecoverEmail = () => {
+//   const auth = getAuth();
+//   const email = document.getElementById("email-recuperar-senha");
+//   sendPasswordResetEmail(auth, email)
+//     .then(() => {
+//       alert("Enviamos um link de recuperação de senha ao seu e-mail!");
+//     })
+//     .catch((error) => {
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       // ..
+//     });
+// };
+
+// Corpo do E-mail
+
+/* <h2>Prezado ${"primeiro nome do usuário"},</h2>
+<p>Nós recebemos um pedido para a mudança de senha de sua conta na R-TEA. Para alterar sua senha, siga o link abaixo:</p>
+
+<p>Se você não fez este pedido, desconsidere este e-mail.</p>
+<p>Nós desenvolvemos esse sistema para você e estamos sempre dispostos a receber seu feedback ou responder a suas perguntas.</p>
+<p>Não responda a este e-mail.</p> */
--- src/modules/firebase/services/auth.ts
+++ src/modules/firebase/services/auth.ts
@@ -1,36 +1,51 @@
-
-import { signInWithEmailAndPassword, getAuth, createUserWithEmailAndPassword, updateProfile, User, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  User,
+  updatePassword,
+  reauthenticateWithCredential,
+  EmailAuthProvider,
+  sendPasswordResetEmail,
+} from "firebase/auth";
 import { firebaseApp } from "../config";
 import { ProfileRepo } from "../repositories/ProfileRepo";
 
 const auth = getAuth(firebaseApp);
 
 export async function login(email: string, password: string) {
-    return await signInWithEmailAndPassword(auth, email, password);
+  return await signInWithEmailAndPassword(auth, email, password);
 }
 
 export async function logout() {
-    return await auth.signOut();
+  return await auth.signOut();
 }
 
-export async function register(email: string, password: string, firstName: string, lastName: string): Promise<void> {
-    const cred = await createUserWithEmailAndPassword(auth, email, password);
+export async function register(
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string
+): Promise<void> {
+  const cred = await createUserWithEmailAndPassword(auth, email, password);
 
-    const profileRepo = new ProfileRepo(cred.user)
-    await profileRepo.updateBasicInfo(firstName, lastName)
+  const profileRepo = new ProfileRepo(cred.user);
+  await profileRepo.updateBasicInfo(firstName, lastName);
 }
 
 export async function setUserAvatar(user: User, photoUrl: string | null) {
-    await updateProfile(user, {
-        photoURL: photoUrl ?? '',
-    });
+  await updateProfile(user, {
+    photoURL: photoUrl ?? "",
+  });
 }
 
-export async function changePassword(user: User, oldPassword: string, newPassword: string) {
-    const oldCredentials = EmailAuthProvider.credential(
-        user.email!,
-        oldPassword
-    )
-    const credential = await reauthenticateWithCredential(user, oldCredentials);
-    await updatePassword(credential.user, newPassword);
+export async function changePassword(
+  user: User,
+  oldPassword: string,
+  newPassword: string
+) {
+  const oldCredentials = EmailAuthProvider.credential(user.email!, oldPassword);
+  const credential = await reauthenticateWithCredential(user, oldCredentials);
+  await updatePassword(credential.user, newPassword);
 }
