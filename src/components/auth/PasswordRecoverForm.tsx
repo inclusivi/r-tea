ad64b0ae139a71e60494071a914ad7f10d8ab1f5
Katiene Souza <katienesouza71@gmail.com>
2024-02-13 21:13:35 -0300

    feat: criação de validação do email com yup

--- src/components/auth/PasswordRecoverForm.tsx
+++ src/components/auth/PasswordRecoverForm.tsx
@@ -1,37 +1,65 @@
-import { Button, Grid, InputLabel, OutlinedInput, Stack } from "@mui/material"
+'use client';
+
+import { Button, FormHelperText, Grid, InputLabel, OutlinedInput, Stack } from "@mui/material"
 import { AnimateButton } from "../shared/elements/AnimatedButton"
+import React from "react";
+import * as Yup from 'yup';
+import { Formik } from "formik";
 
 
 export const PasswordRecoverForm = () => {
     return (
         <>
-            <form>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Stack spacing={1}>
-                            <InputLabel htmlFor="email-recuperar-senha">E-mail</InputLabel>
-                            <OutlinedInput
-                                id="email-recuperar-senha"
-                                type="email"
-                                name="email"
-                                placeholder="Informe seu email"
-                            />
-                        </Stack>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <AnimateButton>
-                            <Button fullWidth size="large" type="submit" variant="contained" color="primary" sx={{ marginBottom: 3 }}>
-                                Enviar
-                            </Button>
-                        </AnimateButton>
-                        <AnimateButton>
-                            <Button fullWidth size="large" type="button" variant="outlined" color="secondary" href="/auth/login">
-                                Cancelar
-                            </Button>
-                        </AnimateButton>
-                    </Grid>
-                </Grid>
-            </form>
+            <Formik
+                initialValues={{
+                    email: ''
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string().email('Deve ser e-mail válido').max(255).required('Favor fornecer seu e-mail')
+                })}
+                onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+
+                }}
+            >
+                {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+                    <form noValidate onSubmit={handleSubmit}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <Stack spacing={1}>
+                                    <InputLabel htmlFor="email-recuperar-senha">E-mail</InputLabel>
+                                    <OutlinedInput
+                                        id="email-recuperar-senha"
+                                        type="email"
+                                        name="email"
+                                        placeholder="Informe seu email"
+                                        value={values.email}
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        error={Boolean(touched.email && errors.email)}
+                                    />
+                                    {touched.email && errors.email && (
+                                        <FormHelperText error id="standard-weight-helper-text-email-recuperacao">
+                                            {errors.email}
+                                        </FormHelperText>
+                                    )}
+                                </Stack>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <AnimateButton>
+                                    <Button disableElevation disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained" color="primary" sx={{ marginBottom: 3 }}>
+                                        Enviar
+                                    </Button>
+                                </AnimateButton>
+                                <AnimateButton>
+                                    <Button fullWidth size="large" type="button" variant="outlined" color="secondary" href="/auth/login">
+                                        Cancelar
+                                    </Button>
+                                </AnimateButton>
+                            </Grid>
+                        </Grid>
+                    </form>
+                )}
+            </Formik>
         </>
     )
 }
