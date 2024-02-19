9346da511a8afbf5d4bd85e843f072d3c1f99efc
Mia Azevedo <mialuzazevedo@gmail.com>
2024-02-18 23:16:14 -0400

    feat: adciona indicador de força de senha e botão de mostrar senha no form de redefinição de senha

--- src/components/auth/PasswordResetForm.tsx
+++ src/components/auth/PasswordResetForm.tsx
@@ -1,15 +1,49 @@
 'use client'
 
-import { Button, FormHelperText, Grid, InputLabel, OutlinedInput, Stack } from "@mui/material"
+import React from 'react'
+
+import { Button, FormHelperText, Grid, InputLabel, OutlinedInput, Stack, Box, Typography, InputAdornment, IconButton } from "@mui/material"
 import { AnimateButton } from "../shared/elements/AnimatedButton"
 
 import * as Yup from 'yup';
 import { Formik } from 'formik';
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
+
+import { strengthColor, strengthIndicator } from '@/modules/utils/passwordStrength';
 
 
-import React from 'react'
+type PasswordStrength = {
+    label: string,
+    color: string
+}
 
 const PasswordResetForm = () => {
+  const [level, setLevel] = React.useState<PasswordStrength>();
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmationPassword, setShowConfirmationPassword] = React.useState(false);
+    
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleClickShowConfirmationPassword = () => {
+    setShowConfirmationPassword(!showConfirmationPassword);
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const changePassword = (value: string) => {
+    const temp = strengthIndicator(value);
+    setLevel(strengthColor(temp));
+  };
+
+  React.useEffect(() => {
+    changePassword('');
+  }, []);
+
+
   return (
     <>
     <Formik
@@ -47,17 +81,47 @@ const PasswordResetForm = () => {
            <Grid container spacing={3}>
              <Grid item xs={12}>
                <Stack spacing={1}>
-                 <InputLabel htmlFor="new-password">Nova senha</InputLabel>
+                 <InputLabel htmlFor="new-password">
+                    <Grid container spacing={2} alignItems="center">
+                      <Grid item sx={{mr: 'auto'}}>
+                          Senha
+                      </Grid>
+                      <Grid item>
+                          <Typography variant="subtitle1" fontSize="0.75rem">
+                              {level?.label}
+                          </Typography>
+                      </Grid>
+                      <Grid item>
+                          <Box sx={{ bgcolor: level?.color, width: 85, height: 8, borderRadius: '7px' }} />
+                      </Grid>
+                    </Grid>
+                  </InputLabel>
                  <OutlinedInput
-                   id="new-password"
-                   type="password"
-                   name="password"
-                   placeholder="Informe sua nova senha"
-                   value={values.password}
-                   onBlur={handleBlur}
-                   onChange={handleChange}
-                   fullWidth
-                   error={Boolean(touched.password && errors.password)}
+                    id="new-password"
+                    type={showPassword ? 'text' : 'password'}
+                    name="password"
+                    placeholder="Informe sua nova senha"
+                    value={values.password}
+                    onBlur={handleBlur}
+                    fullWidth
+                    error={Boolean(touched.password && errors.password)}
+                    onChange={(e) => {
+                      handleChange(e);
+                      changePassword(e.target.value);
+                    }}
+                    endAdornment={
+                      <InputAdornment position="end">
+                          <IconButton
+                              aria-label="toggle password visibility"
+                              onClick={handleClickShowPassword}
+                              onMouseDown={handleMouseDownPassword}
+                              edge="end"
+                              size="large"
+                          >
+                              {showPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                          </IconButton  >
+                      </InputAdornment>
+                  }
                  />
                  {touched.password && errors.password && (
                     <FormHelperText error id="standard-weight-helper-text-new-password">
@@ -72,7 +136,7 @@ const PasswordResetForm = () => {
                  <InputLabel htmlFor='confirmation-password'>Confirmação de senha</InputLabel>
                  <OutlinedInput
                    id="confirmation-password"
-                   type="password"
+                   type={showConfirmationPassword ? 'text' : 'password'}
                    name="confirmationPassword"
                    placeholder="Confirme sua nova senha"
                    value={values.confirmationPassword}
@@ -80,6 +144,19 @@ const PasswordResetForm = () => {
                    onChange={handleChange}
                    fullWidth
                    error={Boolean(touched.confirmationPassword && errors.confirmationPassword)}
+                   endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label="toggle confirmation password visibility"
+                            onClick={handleClickShowConfirmationPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                            size="large"
+                        >
+                            {showConfirmationPassword ? <EyeOutlined /> : <EyeInvisibleOutlined />}
+                        </IconButton  >
+                    </InputAdornment>
+                }
                  />
                  {touched.confirmationPassword && errors.confirmationPassword && (
                     <FormHelperText error id="standard-weight-helper-text-confirmation-password">
@@ -88,6 +165,12 @@ const PasswordResetForm = () => {
                   )} 
                </Stack>
              </Grid>
+
+             {errors.submit && (
+                <Grid item xs={12}>
+                    <FormHelperText error>{errors.submit}</FormHelperText>
+                </Grid>
+              )}
    
              <Grid item xs={12}>
                <Stack spacing={2}>
@@ -103,8 +186,6 @@ const PasswordResetForm = () => {
                  </AnimateButton>
                </Stack>
              </Grid>
-   
-             
    
            </Grid>
          </form>
