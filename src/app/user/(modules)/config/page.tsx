ad90779ead22b3c24acc651d6d46caa74f92e610
RFLac <rogerflacerda@gmail.com>
2024-02-24 10:31:07 -0300

    botoes de senha adicionados, nao funcionam

--- src/app/user/(modules)/config/page.tsx
+++ src/app/user/(modules)/config/page.tsx
@@ -219,7 +219,7 @@ export default function UserConfigPage() {
     const handleClickShowPassword = () => {
       setShowPassword(!showPassword);
     };
-  
+
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
     };
