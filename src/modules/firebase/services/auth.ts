15803bef72485b605eebf23e3e2a8f996f5908d6
michelinfelipe <michelincontato@gmail.com>
2024-02-26 18:51:18 -0300

    Commit teste de tratamento de erro

--- src/modules/firebase/services/auth.ts
+++ src/modules/firebase/services/auth.ts
@@ -4,16 +4,50 @@ import { firebaseApp } from "../config";
 import { ProfileRepo } from "../repositories/ProfileRepo";
 import { User } from "@/modules/user/UserInfo";
 import { updateDocument } from "./database";
+import { FirebaseError } from "firebase/app";
 
 const auth = getAuth(firebaseApp);
-
+console.log("Passou dos 3 imports e do const getauth");
 export async function login(email: string, password: string) {
-    return await signInWithEmailAndPassword(auth, email, password);
-}
+console.log("Exportou");   
+    try { 
+        return await signInWithEmailAndPassword(auth, email, password);
+}  catch (error) {
+            let errorCode = 'unknown';
+            if (error instanceof FirebaseError) {
+                errorCode = error.code;
+            } else {
+                errorCode = String(error) 
+                    }
+            throw new Error(trataMensagemDeErro(errorCode));
+        }
+    }
+    function trataMensagemDeErro(errorCode: string): string{
+        switch (errorCode) {
+            case "auth/invalid-login-credentials":
+                return "Credenciais inválidas";
+            case "auth/user-not-found":
+                return "Usuário não encontrado.";
+            case "auth/invalid-email":
+                return "Email inválido";
+            case "auth/wrong-password":
+                return "Senha incorreta.";
+        //espaço pra colocar mais erros acima
+            default:
+                return "Erro desconhecido: " + errorCode;            
+            }
+    }
+
+
+
 
 export async function logout() {
-    return await auth.signOut();
-}
+    try {
+        return await auth.signOut();
+}   catch (error) {
+        throw new Error("Erro ao fazer logout: " + error.message);
+    }
+    }    
 
 export async function register(email: string, password: string, firstName: string, lastName: string): Promise<void> {
     const cred = await createUserWithEmailAndPassword(auth, email, password);
@@ -37,19 +71,27 @@ export async function register(email: string, password: string, firstName: strin
 }
 
 export async function setUserAvatar(user: FirebaseUser, photoUrl: string | null) {
-    await updateProfile(user, {
-        photoURL: photoUrl ?? '',
+    try {    
+        await updateProfile(user, {
+                photoURL: photoUrl ?? '',
     });
+} catch (error) {
+    throw new Error("Erro ao definir avatar do usuário: ") + error.message;
+    }
 }
 
 export async function changePassword(user: User, oldPassword: string, newPassword: string) {
     const firebaseUser = auth.currentUser
-    const oldCredentials = EmailAuthProvider.credential(
-        user.email!,
-        oldPassword
-    )
-    const credential = await reauthenticateWithCredential(firebaseUser!, oldCredentials);
-    await updatePassword(credential.user, newPassword);
+    try {    
+        const oldCredentials = EmailAuthProvider.credential(
+            user.email!,
+            oldPassword
+        )
+        const credential = await reauthenticateWithCredential(firebaseUser!, oldCredentials);
+        await updatePassword(credential.user, newPassword);
+      } catch (error) {
+            throw new Error ("Erro ao alterar a senha: " + error.message);
+      }        
 }
 
 export async function sendPasswordChangeEmail(email: string) {
