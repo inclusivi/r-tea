72076dec16579bef07bceb3059ca022a6992ef4c
RFLac <rogerflacerda@gmail.com>
2024-02-29 18:39:24 -0300

    título de pessoa sem diagnóstico alterado + títulos dos cards de perfil trocados por variáveis

--- src/modules/firebase/models/UserKind.ts
+++ src/modules/firebase/models/UserKind.ts
@@ -1,31 +1,37 @@
 export enum UserKind {
-    Guest = "guest",
-    Admin = "admin",
-    Responsavel = "responsavel",
-    ProfissionalSaude = "profissional_saude",
-    Cuidador = "cuidador",
-    Educador = "educador",
-    PessoaAutista = "pessoa_autista",
-    PessoaSemDiagnostico = "pessoa_sem_diagnostico",
+  Guest = "guest",
+  Admin = "admin",
+  Responsavel = "responsavel",
+  ProfissionalSaude = "profissional_saude",
+  Cuidador = "cuidador",
+  Educador = "educador",
+  PessoaAutista = "pessoa_autista",
+  PessoaSemDiagnostico = "pessoa_sem_diagnostico",
 }
 
 export const AllUserKinds = [
-    { value: UserKind.Admin, label: 'Administrador' },
-    { value: UserKind.Responsavel, label: 'Responsável' },
-    { value: UserKind.ProfissionalSaude, label: 'Profissional de Saúde' },
-    { value: UserKind.Cuidador, label: 'Cuidador' },
-    { value: UserKind.Educador, label: 'Educador' },
-    { value: UserKind.PessoaAutista, label: 'Pessoa com Autismo' },
-    { value: UserKind.PessoaSemDiagnostico, label: 'Pessoa sem Diagnóstico' },
-]
+  { value: UserKind.Admin, label: "Administrador" },
+  { value: UserKind.Responsavel, label: "Responsável" },
+  { value: UserKind.ProfissionalSaude, label: "Profissional de Saúde" },
+  { value: UserKind.Cuidador, label: "Cuidador" },
+  { value: UserKind.Educador, label: "Educador" },
+  { value: UserKind.PessoaAutista, label: "Pessoa com Autismo" },
+  { value: UserKind.PessoaSemDiagnostico, label: "Jornada de Descoberta" },
+];
 
 export const UserKindDescriptions = {
-    [UserKind.Guest]: 'Convidado',
-    [UserKind.Admin]: 'Administrador',
-    [UserKind.Responsavel]: 'Responsável',
-    [UserKind.ProfissionalSaude]: 'Profissional de Saúde',
-    [UserKind.Cuidador]: 'Cuidador',
-    [UserKind.Educador]: 'Educador',
-    [UserKind.PessoaAutista]: 'Pessoa com Autismo',
-    [UserKind.PessoaSemDiagnostico]: 'Pessoa sem Diagnóstico',
-}
+  [UserKind.Guest]: "Convidado",
+  [UserKind.Admin]: "Administrador",
+  [UserKind.Responsavel]: "Responsável",
+  [UserKind.ProfissionalSaude]: "Profissional de Saúde",
+  [UserKind.Cuidador]: "Cuidador",
+  [UserKind.Educador]: "Educador",
+  [UserKind.PessoaAutista]: "Pessoa com Autismo",
+  [UserKind.PessoaSemDiagnostico]: "Jornada de Descoberta",
+};
+
+// Protótipo do método map para criar array da propriedade label.
+
+// export const UserRotulo = AllUserKinds.map((item) => {
+//   return item.label;
+// });
