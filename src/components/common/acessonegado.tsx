724424fd18be354caccb7e3d5fbc1baa1a521d53
Eduardo-stivanin-c <1sfr2d@gmail.com>
2024-03-11 20:07:39 -0300

    centralizando mensafgem de acesso negado

--- src/components/common/acessonegado.tsx
+++ src/components/common/acessonegado.tsx
@@ -2,7 +2,7 @@
 
 export default function AcessoNegado() {
   return (
-    //pagina de acesso negador no menuque troia ca de tipi ddeusuario
-    <h1>Acesso Negado</h1>
+    //pagina de acesso negador no menu que troia ca de tipi ddeusuario
+   <center><h1>Acesso Negado</h1></center> 
   );
 }
