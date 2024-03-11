68532ff92c973676ce2bab873a3900b5026faf3b
Eduardo-stivanin-c <1sfr2d@gmail.com>
2024-03-11 20:08:11 -0300

    correção de portugues

--- src/components/common/acessonegado.tsx
+++ src/components/common/acessonegado.tsx
@@ -2,7 +2,7 @@
 
 export default function AcessoNegado() {
   return (
-    //pagina de acesso negador no menu que troia ca de tipi ddeusuario
+    //pagina de acesso negador no menu que troia ca de tipo de usuario
    <center><h1>Acesso Negado</h1></center> 
   );
 }
