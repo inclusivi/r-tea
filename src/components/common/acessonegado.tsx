add062634bb002612fb6d612d9039666122a67f4
RFLac <rogerflacerda@gmail.com>
2024-03-12 19:00:48 -0300

    card de informação de acesso negado inserido

--- src/components/common/acessonegado.tsx
+++ src/components/common/acessonegado.tsx
@@ -1,16 +1,12 @@
 "use client";
-import Alert from '@mui/material/Alert';
-//bibloteca tem a uma tag pre consitruoida que fgera html alerta 
+import Alert from "@mui/material/Alert";
+//bibloteca tem a uma tag pre consitruoida que fgera html alerta
 export default function AcessoNegado() {
   //pagina de acesso negador no menu que troia ca de tipo de usuario
   return (
- 
     <center>
-    <Alert severity="info">
-      Acesso Negado
-      
-      </Alert>
-      </center> 
+      <Alert severity="info">Acesso Negado</Alert>
+    </center>
   );
 }
 
